f54be762e85465827722e746517fdf2229e440eb
Sy Mai <mrroboteagle.nexus@gmail.com>
2026-07-17 10:13:43 +0700

    delete test file

--- test.py
+++
@@ -1,8 +0,0 @@
-print("heleo")
-def chao_buoi_sang():
-    print("Chào buổi sáng!") # Đây là callback
-def lam_gi_do(hanh_dong):
-    print("Bắt đầu làm việc...")
-    hanh_dong()  # Bạn đặt lời gọi ở đâu, nó chạy đúng ở vị trí đó
-    print("Làm việc xong!")
-lam_gi_do(chao_buoi_sang)
